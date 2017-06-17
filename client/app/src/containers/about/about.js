093084f17906e9106b4b6a08d6dcf4d3304810ec
Nir Barzilay <nbtk123@gmail.com>
2017-06-17 12:47:03 +0300

    (client): updated 'about' text

--- client/app/src/containers/about/about.js
+++ client/app/src/containers/about/about.js
@@ -4,8 +4,18 @@ import './about.css';
 class About extends Component {
     render() {
         return (
-            <div className="about primary-bg-color">
-                <h2>Bacon ipsum dolor amet biltong chuck turducken, burgdoggen boudin cow pancetta t-bone corned beef pig. Hamburger ground round pork belly meatball alcatra. Andouille cupim salami kielbasa. Drumstick cow biltong tenderloin short loin pork, ribeye spare ribs ground round cupim jowl pastrami prosciutto beef.</h2>
+            <div className="about primary-bg-color primary-text-color">
+                <h3><p>Cooperating with Israel’s Ministry of Foreign Affairs and The National Union of Israeli
+Students, The program allows students to take an active part against online
+antisemitism, racism and holocaust denial.</p>
+
+<p>The program accepts students from all faculties (Hebrew speakers only), who are highly
+motivated to tackle anti-Semitism, have a good understanding of online media and
+possess the ability to work independently.</p>
+
+<p>For additional details:<br/>
+Adi Goldstein, Program coordinator – isca@idc.ac.il<br/>
+Roy Meirom, Program Director – roy.meirom@idc.ac.il</p></h3>
             </div>
         );
     }
