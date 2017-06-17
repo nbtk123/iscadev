c159dbce057564cca5c8d584e89c04c295f77565
Nir Barzilay <nbtk123@gmail.com>
2017-06-17 12:01:45 +0300

    (client) : updated statistics info

--- client/app/src/containers/statistics/statistics.js
+++ client/app/src/containers/statistics/statistics.js
@@ -7,10 +7,12 @@ class Statistics extends Component {
     render() {
         return (
             <div className="container-fluid secondary-bg-color secondary-text-color statistics-container">
-                <div className="col-md-3 statistics-circle-padding"><TextInCircle topText="10+" bottomText="HOURS PER DAY"/></div>
-                <div className="col-md-3 statistics-circle-padding"><TextInCircle topText="10+" bottomText="HOURS PER DAY"/></div>
-                <div className="col-md-3 statistics-circle-padding"><TextInCircle topText="10+" bottomText="HOURS PER DAY"/></div>
-                <div className="col-md-3 statistics-circle-padding"><TextInCircle topText="10+" bottomText="HOURS PER DAY"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="15,000+" bottomText="FACEBOOK REPORTS"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="7,300+" bottomText="TWITTER REPORTS"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="6,300+" bottomText="TWITTER REPORTS"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="4,300+" bottomText="YOUTUBE REPORTS"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="3,000+" bottomText="YAHOO QUESTIONS"/></div>
+                <div className="col-md-2 statistics-circle-padding"><TextInCircle topText="2,200+" bottomText="WIKIPEDIA ACTIVITIES"/></div>
             </div>
         );
     }
