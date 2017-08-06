31c7b5e8cd40b17da318a38b33537b415825b268
Nir Barzilay <nbtk123@gmail.com>
2017-08-07 01:32:38 +0300

    (client/contactus): created dummy typeform

--- src/containers/contactus/contactus.js
+++ src/containers/contactus/contactus.js
@@ -34,8 +34,17 @@ class ContactUs extends Component {
                     <h2 className="primary-text-color" style={{'textAlign':'center'}}>FAQ</h2>
                     <QuestionList />
                     <hr className="faq-seperator"/>
-                    <h2 className="primary-text-color" style={{'textAlign':'center'}}>Leave a message</h2>
-                    <ContactUsForm />
+                    {/*<h2 className="primary-text-color" style={{'textAlign':'center'}}>Leave a message</h2>
+                    <ContactUsForm />*/}
+
+                    <div className="typeform-widget" data-url="https://iscadev.typeform.com/to/mDfesN" style={{width: '100%', height: '500px'}} >
+                    </div>
+                    <script> {
+                        (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
+                        }
+                    </script>
+                    <div style={{fontFamily: 'Sans-Serif', fontSize: '12px', color: '#999', opacity: '0.5', paddingTop: '5px'}} > powered by <a href="https://www.typeform.com/examples/forms/contact-form-template/?utm_campaign=mDfesN&amp;utm_source=typeform.com-8482275-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-contactform&amp;utm_term=EN" style={{color: '#999'}} target="_blank">Typeform</a> </div>
+                        
                 </div>
         );
     }
