746ac872222fc12219853e5a947329ca9387c5b2
nbtk <nbtk123@gmail.com>
2017-08-11 14:06:45 +0300

    (client/team): ui improvements

--- src/containers/team/teammember2.js
+++ src/containers/team/teammember2.js
@@ -6,45 +6,13 @@ class TeamMember2 extends Component {
 
   constructor(props) {
     super(props);
-    
-    this.onMouseOver = this.onMouseOver.bind(this);
-    this.onMouseOut = this.onMouseOut.bind(this);
-  }
-
-  onMouseOver(element) {
-    // var target = element.target;
-
-    // // Find the image container
-    // while (target.className != 'our-team') {
-    //   target = target.parentElement;
-    // }
-
-    // // set top-margin to current screen location
-    // target.style.marginTop = target.style.top;
-    // target.style.position = 'absolute'
-    // console.log(target.className)
-  }
-
-  onMouseOut(element) {
-    // var target = element.target;
-
-    // // Find the image container
-    // while (target.className != 'our-team') {
-    //   target = target.parentElement;
-    // }
-    
-    // //target.style.position = 'relative'
-
-    // // set top-margin to current screen location
-    // target.style.marginTop = 0;
-    // console.log(target.className)
   }
 
   render() {
 
     return (
         <div className="col-md-1">
-            <div className="our-team" onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+            <div className="our-team">
                 <img src={this.props.imgsrc} />
                 <div className="team-content">
                     <h3 className="title secondary-font-family">{this.props.name}</h3>
